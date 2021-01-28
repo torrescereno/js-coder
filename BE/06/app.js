fce23b336067f7e255d62bd49cae367f6bf79751
Luis <torrescereno@gmail.com>
2021-01-27 22:36:44 -0300

    desafio 06 v1

--- BE/06/app.js
+++ BE/06/app.js
@@ -1,37 +1,5 @@
+
 const fs = require('fs');
-const { async } = require('rxjs');
-
-class Archivo {
-
-    constructor(nombre) {
-        this.nombre = nombre;
-    }
-
-    async leer() {
-        try {
-            return await fs.promises.readFile(this.nombre, 'utf-8') || [];
-        } catch (error) {
-            return 'No existe el archivo';
-        }
-    }
-
-    async guardar(producto) {
-
-        const data = await this.leer();
-        
-        return data
-    }
-
-    async borrar() {
-        try {
-            await fs.promises.unlink(this.nombre);
-            return `Archivo ${miArchivo.nombre} borrado`;
-        } catch (error) {
-            return 'Error al borrar el archivo';
-        }
-    }
-
-};
 
 const producto = {
     title: 'Libro',
@@ -41,21 +9,66 @@ const producto = {
 
 const archivo = 'productos.txt';
 
+class Archivo {
+
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+
+    async leer() {
+        try {
+            const data = await fs.promises.readFile(this.nombre, 'utf-8') || [];
+            console.log(data)
+        } catch (err) {
+            console.log('No existe el archivo', err);
+        }
+    }
+
+    async guardar(producto) {
+
+        try {
+
+            // Obtener el contenido del archivo
+            let data = await fs.promises.readFile(this.nombre, 'utf-8') || [];
+            data = JSON.parse(data);
+
+            // Obtener el largo del arreglo
+            const totalArray = data.length;
+
+            // Agregar el objeto con el ID
+            producto.id = totalArray + 1;
+
+            // Push al arreglo
+            data.push(producto)
+
+            await fs.promises.writeFile(this.nombre, JSON.stringify(data), 'utf-8');
+            console.log(`Archivo actuializado`);
+
+        } catch (err) {
+            console.log('no se pudo actualizar', err)
+        }
+
+    }
+
+    async borrar() {
+        try {
+            await fs.promises.unlink(this.nombre);
+            console.log(`Archivo ${miArchivo.nombre} borrado`);
+        } catch (err) {
+            console.log('Error al borrar el archivo', err);
+        }
+    }
+};
+
 const miArchivo = new Archivo(archivo);
 
-async function leerArchivo() {
-    console.log(await miArchivo.leer());
+
+async function operarArchivo(producto) {
+
+   await miArchivo.leer();
+   await miArchivo.guardar(producto);
+   await miArchivo.borrar();
+        
 }
 
-async function guardarArhivo() {
-    const data = await miArchivo.guardar(producto);
-    console.log(data);
-}
-
-async function borrarArchivo() {
-    console.log(await miArchivo.borrar());
-}
-
-//leerArchivo();
-guardarArhivo();
-// borrarArchivo();
+operarArchivo(producto);
