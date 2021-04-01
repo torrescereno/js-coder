980c84bd713967b5d39c81b502d8ca60952df262
Luis Torres Cereño <torrescereno@gmail.com>
2021-03-31 22:32:52 -0300

    server

--- BE/14/dist/server.es6.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-server.listen(PORT, function () {
-    console.log('http://localhost:' + PORT + '/');
-});
